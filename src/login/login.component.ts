a87a7de6880399fe398cc8e7edc9e46533b956d9
ammarRBK <alshakmaskeain@gmail.com>
2023-11-20 01:04:27 +0300

    reactive forms for login page worked now

--- src/login/login.component.ts
+++ src/login/login.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  imports: [ReactiveFormsModule, FormsModule]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginInterfaceForm: FormGroup;
 
+  constructor(private loginForm: FormBuilder, private router: Router){}
+  ngOnInit(): void {
+    this.loginInterfaceForm= this.loginForm.group({
+      email: ['', [Validators.email, Validators.required]],
+      password: ['',[Validators.required]]
+    })
+  }
+
+  submitLogin(){
+    console.log(this.loginInterfaceForm.value);
+  }
 }
