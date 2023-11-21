292775102a3a993b28922e69c4d5f6dd85489109
ammarRBK <alshakmaskeain@gmail.com>
2023-11-21 19:12:51 +0300

    dashboard component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from 'src/login/login.component';
+import { LoginComponent } from '../login/login.component';
+import { DashboardComponent } from 'src/login/dashboard/dashboard.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,14 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'login/dashboard',
+    component: DashboardComponent
   }
 ];
 
