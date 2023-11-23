a06ed38f9b261ea347ffdd5bed35fc1c20a134d0
ammarRBK <alshakmaskeain@gmail.com>
2023-11-23 08:37:35 +0300

    getting dashboard data and showing in table worked fine but needs some UI styling

--- src/login/dashboard/dashboard.component.ts
+++ src/login/dashboard/dashboard.component.ts
@@ -1,19 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { FirebaseserviceService } from 'src/services/firebaseservice.service';
 
 @Component({
+  standalone: true,
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
+  imports: [CommonModule]
 })
 export class DashboardComponent implements OnInit {
 
+  completedVisits: any;
   constructor(private fservice:FirebaseserviceService){}
   ngOnInit(): void {
         this.fservice.getCompletedvisits().then(visits=>{
-          console.log(visits);
+          this.completedVisits= visits;
         })
   }
 
